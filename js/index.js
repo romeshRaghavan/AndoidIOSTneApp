dddfe1a544757665894848857f6749277f5580e5
DineshSonar <magneto.1990@gmail.com>
2017-11-30 11:08:48 +0530

    test

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ var expensePageFlag = '';
 var filtersStr = "";
  j(document).ready(function(){ 
     document.addEventListener("deviceready",loaded,false);
-   setInterval(getSms, 10*1000);
+   //setInterval(getSms, 10*1000);
   //setInterval(getSms, 50);
     
 }); 
